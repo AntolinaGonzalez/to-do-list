48e0bf17faa3bab35a6bb99d2ddc56287b300306
Antolina Gonzalez <antolina487@gmail.com>
2021-04-18 20:20:08 -0300

    head and favico

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,19 @@
 import "../styles/globals.css";
 import { MuiThemeProvider } from "@material-ui/core/styles";
 import {theme} from'../theme'
+import Head from 'next/head'
+
 function MyApp({ Component, pageProps }) {
   return (
+    <>
+     <Head>
+        <title>ToDo</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
     <MuiThemeProvider theme={theme}>
       <Component {...pageProps} />
     </MuiThemeProvider>
+    </>
   );
 }
 
